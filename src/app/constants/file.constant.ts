703a7798def0629461db4fc8b7dfc49d18cfc7e4
iibrahiim70 <iibrahiim.dev@gmail.com>
2024-08-21 15:37:04 +0600

    :hammer: added constants for file uploading

---
+++ src/app/constants/file.constant.ts
@@ -0,0 +1,23 @@
+export const IMAGE_FIELD_NAMES = [
+  'avatar',
+  'propertyImages',
+  'proofOfOwnership',
+];
+export const VIDEO_FIELD_NAMES = ['propertyVideo'];
+export const AUDIO_FIELD_NAMES = ['audio'];
+export const PDF_FIELD_NAMES = ['pdf'];
+
+export const SUPPORTED_IMAGE_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
+export const SUPPORTED_VIDEO_FORMATS = ['video/mp4'];
+export const SUPPORTED_AUDIO_FORMATS = ['audio/mpeg'];
+export const SUPPORTED_PDF_FORMATS = ['application/pdf'];
+
+// Create a map for field names and their corresponding supported formats
+export const FIELD_NAME_TO_FORMATS: { [key: string]: string[] } = {
+  avatar: SUPPORTED_IMAGE_FORMATS,
+  propertyImages: SUPPORTED_IMAGE_FORMATS,
+  proofOfOwnership: SUPPORTED_IMAGE_FORMATS,
+  propertyVideo: SUPPORTED_VIDEO_FORMATS,
+  audio: SUPPORTED_AUDIO_FORMATS,
+  pdf: SUPPORTED_PDF_FORMATS,
+};
