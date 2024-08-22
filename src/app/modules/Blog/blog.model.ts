7dbc039cfa1f6b1e88a35d437411b29504ff007f
iibrahiim70 <iibrahiim.dev@gmail.com>
2024-08-22 08:49:13 +0600

    :hammer: added model for blog

---
+++ src/app/modules/Blog/blog.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+import { IBlog } from './blog.interface';
+
+const blogSchema = new Schema<IBlog>(
+  {
+    createdBy: {
+      type: Schema.Types.ObjectId, // MongoDB ObjectId type
+      ref: 'User', // Reference to the 'User' model
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    blogImage: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+// Create the Blog model using the schema
+export const Blog = model<IBlog>('Blog', blogSchema);
